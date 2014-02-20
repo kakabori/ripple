622b5e6efb825d8b0b8874354da7becc74dc7823
Kiyo <kakabori@andrew.cmu.edu>
2014-02-19 21:46:42 -0500

    first commit

---
+++ ripple_LAXS.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib as ppl
+import scipy.optimize
+
+def read_data(fileobj, skip=0):
+  """
+  
+  """
+  # ignore the first skip lines
+  for i in range(skip):
+    fileobj.readline()
+  
+  h = []; k = []; q = []; F = []
+  lines = fileobj.readlines()
+  for line in lines:
+    hval, kval, qval, Fval = line.split()
+    h.append(hval); k.append(kval); q.append(qval); F.append(Fval)
+  
+  harr = np.array(h, int)
+  karr = np.array(k, int)
+  qarr = np.array(q, float)
+  Farr = np.array(F, float)  
+  return harr, karr, qarr, Farr
+  
+
+class Ripple:
+  """ The ripple class for fitting LAXS data and getting electron density 
+  profile. """
+  
+  def __init__(self, h, k, q, F):
+    self.hkqF = []
+    for i in range(len(h)):
+      tmp = [h[i], k[i], q[i], F[i]]
+      self.hkqF.append(tmp)
+              
+  def fit(self):
+    x = np.array(self.hkqF[:,:2], int)
+    y = self.hkqF[:,2] * self.hkqF[:,2]    
+    scipy.optimize.curve_fit(func, x, y)    
+
+
+def q_x(k, lambda_r):
+  """
+  Return qx value in the ripple phase LAXS.
+  k: k index
+  lambda_r: lambda_r, the ripple wavelength
+  """
+  return 2*np.pi*k/lambda_r
+
+
+def q_z(h, k, D, lambda_r, gamma):
+  """
+  Return qz value in the ripple phase LAXS.
+  h: h index
+  k: k index
+  D: D-spacing 
+  lambda_r: lambda_r, the ripple wavelength
+  gamma: gamma angle of the unit cell
+  """
+  return 2*np.pi*(h/D - k/lambda_r/np.tan(gamma))
+
+
+def q_square(h, k, D, l, g):
+  """ 
+  Return q = qx^2 + qz^2 in the ripple phase LAXS.
+  h: h index
+  k: k index
+  D: D-spacing 
+  l: lambda_r, the ripple wavelength
+  g: gamma angle of the unit cell
+  """
+  return q_x(k, l)*q_x(k, l) + q_z(h, k, D, l, g)*q_z(h, k, D, l, g)    
+
+
+def func(x, D, lambda_r, gamma):
+  """ x is an n-by-2 array, where the first column holds h values and the 
+  second column k values. n is the number of data points. D, lambda_r, and 
+  gamma are fitting parameters. """
+  return q_square(x[:,0], x[:,1], D, lambda_r, gamma)
+
+
+if __name__ == 'main':
+  filename = 'WackWebb.dat'
+  infile = open(filename, 'r')
+  h, k, q, F = read_data(infile, 1)
+  obj = Ripple(h, k, q, F)
+  
+  
+filename = 'WackWebb.dat'
+infile = open(filename, 'r')
+h, k, q, F = read_data(infile, 1)
+myobj = Ripple(h, k, q, F)
