47338edf8e16cf3f9d83858c65e2cbe74626bf60
Kiyo <kakabori@andrew.cmu.edu>
2014-03-17 00:07:14 -0400

    implementing MDF. f2 term does not seem to work. Should look up the thesis.

--- ripple_LAXS.py
+++ ripple_LAXS.py
@@ -59,21 +59,6 @@ class Ripple:
   def update(self):
     self.qx = q_x(self.k, self.lambda_r)
     self.qz = q_z(self.h, self.k, self.D, self.lambda_r, self.gamma)
-    
-  
-  def fit_SDF(self):
-    pass
-  
-  
-  def fit_SGF(self):  
-    pass
-  
-  
-def omega(qx, qz, x0, A):
-  """
-  Return the intermediate variable, omega
-  """
-  return 0.5 * (qx*x0 + qz*A)
         
         
 def q_x(k, lambda_r):
@@ -125,24 +110,16 @@ def func(x, D, lambda_r, gamma):
   return q_square(x[0], x[1], D, lambda_r, gamma)
 
 
-def F_trans(qx, qz, X_h, psi, rho_M, R_HM):
-  arg = qz*X_h*np.cos(psi) - qx*X_h*np.sin(psi)
-  return rho_M * (R_HM*np.cos(arg) - 1)
-
-
-def F_cont(qx, qz, x0, A, lambda_r):
-  w = omega(qx, qz, x0, A)
-  lr = lambda_r
-  arg1 = 0.5*qx*lr + w
-  arg2 = 0.5*qx*lr - w
-  fir = x0 * np.sin(w) / lambda_r / w
-  sec = (lr-x0) * np.cos(0.5*arg1) * np.sin(arg2) / lr / np.cos(0.5*arg2) / arg2 
-  return (fir + sec)
-
+def omega(qx, qz, x0, A):
+  """
+  Return the intermediate variable, omega
+  """
+  return 0.5 * (qx*x0 + qz*A)
+  
 
 def SDF_model(params, qx, qz):
   """
-  Return the simple delta function. The return object is an numpy array with
+  Return the simple delta function. The return object is a numpy array with
   its length equal to the length of qx.
   
   params: lmfit Parameter object, requires x0, A, rho_M, R_HM, X_h, psi, and
@@ -157,30 +134,80 @@ def SDF_model(params, qx, qz):
   psi = params['psi'].value
   lambda_r = params['lambda_r'].value
 
-  model = F_trans(qx, qz, X_h, psi, rho_M, R_HM) * F_cont(qx, qz, x0, A, lambda_r)
+  model = SDF_F_trans(qx, qz, X_h, psi, rho_M, R_HM) * \
+          SDF_F_cont(qx, qz, x0, A, lambda_r)
 
   return model
   
+
+def SDF_F_trans(qx, qz, X_h, psi, rho_M, R_HM):
+  arg = qz*X_h*np.cos(psi) - qx*X_h*np.sin(psi)
+  return rho_M * (R_HM*np.cos(arg) - 1)
+  
+
+def SDF_F_cont(qx, qz, x0, A, lambda_r):
+  w = omega(qx, qz, x0, A)
+  lr = lambda_r
+  arg1 = 0.5*qx*lr + w
+  arg2 = 0.5*qx*lr - w
+  fir = x0 * np.sin(w) / lambda_r / w
+  sec = (lr-x0) * np.cos(0.5*arg1) * np.sin(arg2) / lr / np.cos(0.5*arg2) / arg2 
+  return (fir + sec)
+
 
 def MDF_model(params, qx, qz):
   """
   
   """
-  pass
+  x0 = params['x0'].value
+  A = params['A'].value
+  rho_M = params['rho_M'].value
+  R_HM = params['R_HM'].value
+  X_h = params['X_h'].value
+  psi = params['psi'].value
+  lambda_r = params['lambda_r'].value
+  f1 = params['f1'].value
+  f2 = params['f2'].value
   
+  model = MDF_F_trans(qx, qz, X_h, psi, rho_M, R_HM) * \
+          MDF_F_cont(qx, qz, x0, A, lambda_r, f1, f2)
+  
+  return model
 
+def MDF_F_trans(qx, qz, X_h, psi, rho_M, R_HM):
+  """
   
+  """
+  return SDF_F_trans(qx, qz, X_h, psi, rho_M, R_HM)
+
+
+def MDF_F_cont(qx, qz, x0, A, lambda_r, f1, f2):
+  """
+  
+  """
+  w = omega(qx, qz, x0, A)
+  lr = lambda_r
+  arg1 = 0.5*qx*lr + w
+  arg2 = 0.5*qx*lr - w
+  fir = x0 * np.sin(w) / lambda_r / w
+  sec = (lr-x0) * np.cos(0.5*arg1) * np.sin(arg2) / lr / np.cos(0.5*arg2) / arg2 
+  return (fir + f1*sec + 2*f2*np.cos(w)) 
+
+    
 # define objective function: returns the array to be minimized
-def residual(params, qx, qz, data=None, sigma=None):
+def residual(params, model_type, qx, qz, data=None, sigma=None):
   """
   Return the individual residuals. If data=None, return the model. If 
   simga=None, return (data - model). Otherwise, return (data-model) / sigma.
-  model is the form factor squared, that is, intensity.
+  model is the form factor squared, that is, intensity. model_type can take
+  'SDF' or 'MDF'.
   """
   
-  # calculate intensity = form factor squared
-  model = np.absolute(SDF_model(params, qx, qz))
-  
+  if model_type == 'SDF':
+    model = np.absolute(SDF_model(params, qx, qz))
+  elif model_type == 'MDF':
+    model = np.absolute(MDF_model(params, qx, qz))
+    
   if data is None:
     return model
   if sigma is None:
@@ -188,6 +215,9 @@ def residual(params, qx, qz, data=None, sigma=None):
   return (data-model) / sigma
 
 
+
+  
+  
 def Fourier_decomp(N=201, xmin=-100, xmax=100, zmin=-100, zmax=100):
   rho_xz = []
   xgrid = np.linspace(xmin, xmax, num=N)
@@ -206,8 +236,12 @@ def Fourier_decomp(N=201, xmin=-100, xmax=100, zmin=-100, zmax=100):
   plt.contourf(X, Y, Z)
 
 
-def get_phase(params, qx, qz):
-  tmp = SDF_model(params, qx, qz)
+def get_phase(params, qx, qz, model_type):
+  if model_type == 'SDF':
+    tmp = SDF_model(params, qx, qz)
+  elif model_type == 'MDF':
+    tmp = MDF_model(params, qx, qz)
+    
   return np.sign(tmp)
 
 
@@ -217,7 +251,27 @@ def show_model():
   for a, b, c, d, e, f, g in zip(h, k, qx, qz, q, model, F):
     print("{0: 1d} {1: 1d}  {2: .3f} {3: .3f} {4: .3f} {5: 7.2f} {6: 7.2f}"
           .format(a, b, c, d, e, f, g))
-      
+
+
+def show_profile(start, end):
+  """
+  Show Fourier decomposed EDP along a line connecting the start and end points
+  """
+  rho = []
+  x0, z0 = start
+  x1, z1 = end
+  N = 100 # number of points on which ED gets calculated
+  xpoints = np.linspace(x0, x1, N)
+  zpoints = np.linspace(z0, z1, N)
+  for x, z in zip(xpoints, zpoints):
+    tmp = phase * F * np.cos(qx*x+qz*z)
+    dist = np.sqrt((x-x0)**2 + (z-z0)**2)
+    rho.append([dist, tmp.sum(axis=0)])
+  rho = np.array(rho, float)
+  X = rho[:,0]
+  Y = rho[:,1]
+  plt.plot(X, Y)
+  
   
 if __name__ == "__main__":
   # read data to be fitted
@@ -241,33 +295,37 @@ if __name__ == "__main__":
   
   # Work on SDF
   params = Parameters()
-  params.add('x0', value=60.0, vary=True)
-  params.add('A', value=20.0, vary=True)
-  params.add('rho_M', value=10.0, vary=True)
-  params.add('R_HM', value=2.0, vary=True)
-  params.add('X_h', value=20.0, vary=True)
-  params.add('psi', value=0.14, vary=True)
-  params.add('lambda_r', value = lambda_r, vary=False)
+  params.add('x0', value=105, vary=True)
+  params.add('A', value=20.3, vary=True)
+  params.add('rho_M', value=53.9, vary=True)
+  params.add('R_HM', value=2.21, vary=True)
+  params.add('X_h', value=20.2, vary=True)
+  params.add('psi', value=0.0868, vary=True)
+  params.add('lambda_r', value=lambda_r, vary=False)
   
   x = np.array(q, float)
   data = np.array(F, float)
-  result = minimize(residual, params, args=(qx, qz, data))
+  result = minimize(residual, params, args=('SDF', qx, qz, data))
   lmfit.report_fit(params)
-  phase = get_phase(params, qx, qz)
+  print("chisqr = {0:.3f}".format(result.chisqr))
+  phase = get_phase(params, qx, qz, 'SDF')
   
-  # Work on SGF
-  params2 = Parameters()
-  params2.add('x0', value=20, vary=True)
-  params2.add('A', value=20, vary=True)
-  params2.add('rho_M', value=10, vary=True)
-  params2.add('R_HM', value=10, vary=True)
-  params2.add('X_h', value=20, vary=True)
-  params2.add('psi', value=0, vary=True)
-  params2.add('lambda_r', value = lambda_r, vary=False)
+  # Work on MDF
+  params = Parameters()
+  params.add('x0', value=103, vary=True)
+  params.add('A', value=20, vary=True)
+  params.add('rho_M', value=60, vary=True)
+  params.add('R_HM', value=2, vary=True)
+  params.add('X_h', value=20.4, vary=True)
+  params.add('psi', value=0.157, vary=True)
+  params.add('lambda_r', value=lambda_r, vary=False)
+  params.add('f1', value=0.7, vary=True)
+  params.add('f2', value=0, vary=True)
   
-  result2 = minimize(residual2, params2, args=(qx, qz, data))
-  lmfit.report_fit(params2)
-  phase2 = get_phase(params2, qx, qz)
+  result = minimize(residual, params, args=('MDF', qx, qz, data))
+  lmfit.report_fit(params)
+  print("chisqr = {0:.3f}".format(result.chisqr))
+  phase = get_phase(params, qx, qz, 'MDF')
   
   
   # Work on S1G
