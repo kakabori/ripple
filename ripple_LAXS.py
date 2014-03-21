9583d7e942fd2df6caa944c52659f04ad090ee08
Kiyo <kakabori@andrew.cmu.edu>
2014-03-21 09:35:14 -0400

    commit

--- ripple_LAXS.py
+++ ripple_LAXS.py
@@ -24,11 +24,12 @@ def read_data(fileobj, skip=0):
   
 
 ###############################################################################
-class BaseRipple:
+class BaseRipple(object):
   """ 
   The base ripple class, which will be inherited by contour subclasses, which
-  in turn will be inherited by transbilayer subclasses. This base class only
+  in turn will be inherited by transbilayer subclasses. This base class mainly
   deals with the ripple lattice parameters, namely, D, lambda_r, and gamma.
+  Methods such as showing 1D and 2D edp are also implemented.
   """
   def __init__(self, h, k, F, q=None, qx=None, qz=None, D=58, lambda_r=140, 
                gamma=1.7):
@@ -43,22 +44,80 @@ class BaseRipple:
     self.latt_par.add('lambda_r', value=lambda_r, vary=True)
     self.latt_par.add('gamma', value=gamma, vary=True)
 
-  def set_phase(self):
+  def _set_phase(self):
     """
     model method needs to be defined in the subclasses
     """
-    self.phase = np.sign(self.model())
+    self.phase = np.sign(self._model())
+    
+  def show_2D_edp(self, N=201, xmin=-100, xmax=100, zmin=-100, zmax=100):
+    """
+    Fourier-reconstruct a 2D map of the electron density profile. Calculate
+    EDP at N points along x and N points along z. The units are in Angstrom.
+    """
+    rho_xz = []
+    xgrid = np.linspace(xmin, xmax, num=N)
+    zgrid = np.linspace(zmin, zmax, num=N)
+    for x in xgrid:
+      for z in zgrid:
+        tmp = self.phase * self.F * np.cos(self.qx*x+self.qz*z)
+        rho_xz.append([x, z, tmp.sum(axis=0)])
+    rho_xz = np.array(rho_xz, float)  
+    X, Y, Z= rho_xz[:,0], rho_xz[:,1], rho_xz[:,2]
+    #Y = rho_xz[:,1]
+    #Z = rho_xz[:,2]
+    X.shape = (N, N)
+    Y.shape = (N, N)
+    Z.shape = (N, N)
+    plt.contourf(X, Y, Z)
+    
+  def show_1D_edp(self, start=(-10,25), end=(30,-20), N=100):
+    """
+    Show Fourier-reconstructed EDP along a line connecting the start and end points
+    N: number of points on which ED gets calculated
+    Call plt.show() to actually display the plot.
+    """
+    rho = []
+    x0, z0 = start
+    x1, z1 = end
+    xpoints = np.linspace(x0, x1, N)
+    zpoints = np.linspace(z0, z1, N)
+    for x, z in zip(xpoints, zpoints):
+      tmp = self.phase * self.F * np.cos(self.qx*x+self.qz*z)
+      dist = np.sqrt((x-x0)**2 + (z-z0)**2)
+      rho.append([dist, tmp.sum(axis=0)])
+    rho = np.array(rho, float)
+    X = rho[:,0]
+    Y = rho[:,1]
+    plt.plot(X, Y)
+    
+  def report_model_F(self):
+    """
+    Show the model form factor along with the experimental |F|. Need a model
+    method to call, which should be implemented in a derived class.
+    """
+    print(" h  k      qx     qz      q   model       F")
+    for a, b, c, d, e, f, g in zip(self.h, self.k, self.qx, self.qz, self.q, 
+                                   self._model(), self.F):
+      print("{0: 1d} {1: 1d}  {2: .3f} {3: .3f} {4: .3f} {5: 7.2f} {6: 7.2f}"
+            .format(a, b, c, d, e, f, g))
   
   def report_lattice(self):
+    """
+    Report the best fit lattice parameters
+    """
     lmfit.report_fit(self.latt_par)
     print("chisqr = {0:.3f}".format(self.lattice.chisqr))
           
   def fit_lattice(self):
-    self.lattice = minimize(self.residual_lattice, self.latt_par)    
+    """
+    Start a non-linear least squared fit for lattice parameters.
+    """
+    self.lattice = minimize(self._residual_lattice, self.latt_par)    
     self.qx = self._q_x()
     self.qz = self._q_z()
 
-  def residual_lattice(self, params):
+  def _residual_lattice(self, params):
     """
     params is a dummy variable, necessary for lmfit.minimize to work
     """
@@ -94,13 +153,44 @@ class BaseRipple:
     lambda_r = self.latt_par['lambda_r'].value
     gamma = self.latt_par['gamma'].value
     return 2*np.pi*(self.h/D - self.k/lambda_r/np.tan(gamma))
+    
+  def report_edp(self):
+    """
+    Report best fit parameters related to electron density profile
+    """
+    lmfit.report_fit(self.edp_par)
+    print("chisqr = {0:.3f}".format(self.edp.chisqr))
+        
+  def fit_edp(self):
+    """
+    Start a non-linear least squared fit for electron density profile
+    """
+    self.edp = minimize(self._residual, self.edp_par)
+    self._set_phase()
+    
+  def _residual(self, params):
+    """
+    Return the individual residuals.  
+    """
+    data = self.F
+    model = np.absolute(self._model())
+    return (data - model) 
+    #return (data-model) / sigma    
+    
+  def _model(self):
+    """
+    Return the model form factor. The return object is a numpy array with
+    its length equal to the length of qx.
+    """
+    model = self.F_trans() * self.F_cont()
+    return model
   
 
 ###############################################################################
 class Sawtooth(BaseRipple):
   def __init__(self, h, k, F, q=None, qx=None, qz=None, D=58, lambda_r=140, 
                gamma=1.7, x0=100, A=20):
-    super().__init__(h, k, F, q, qx, qz, D, lambda_r, gamma)
+    super(Sawtooth, self).__init__(h, k, F, q, qx, qz, D, lambda_r, gamma)
     self.edp_par = Parameters()
     self.edp_par.add('x0', value=x0, vary=True)
     self.edp_par.add('A', value=A, vary=True)
@@ -116,14 +206,14 @@ class Sawtooth(BaseRipple):
     f1 = self.edp_par['f1'].value
     f2 = self.edp_par['f2'].value
     lr = self.latt_par['lambda_r'].value
-    w = self.omega()
+    w = self._omega()
     arg1 = 0.5*self.qx*lr + w
     arg2 = 0.5*self.qx*lr - w
     fir = x0 * np.sin(w) / lr / w
     sec = (lr-x0) * np.cos(0.5*arg1) * np.sin(arg2) / lr / np.cos(0.5*arg2) / arg2 
     return (fir + f1*sec + 2*f2*np.cos(w)) 
   
-  def omega(self):
+  def _omega(self):
     """
     Return the intermediate variable, omega
     """
@@ -137,38 +227,16 @@ class SDF(Sawtooth):
   def __init__(self, h, k, F, q=None, qx=None, qz=None, D=58, lambda_r=140, 
                gamma=1.7, x0=100, A=20, rho_M=20, R_HM=2, X_h=20, 
                psi=0.087):
-    super().__init__(h, k, F, q, qx, qz, D, lambda_r, gamma, x0, A)
+    super(SDF, self).__init__(h, k, F, q, qx, qz, D, lambda_r, gamma, x0, A)
     self.edp_par.add('rho_M', value=rho_M, vary=True)
     self.edp_par.add('R_HM', value=R_HM, vary=True)
     self.edp_par.add('X_h', value=X_h, vary=True)
     self.edp_par.add('psi', value=psi, vary=True)
   
-  def report_edp(self):
-    lmfit.report_fit(self.edp_par)
-    print("chisqr = {0:.3f}".format(self.edp.chisqr))
-        
-  def fit_edp(self):
-    self.edp = minimize(self.residual, self.edp_par)
-    self.set_phase()
-    
-  def residual(self, params):
-    """
-    Return the individual residuals.  
-    """
-    data = self.F
-    model = np.absolute(self.model())
-    return (data - model) 
-    #return (data-model) / sigma    
-    
-  def model(self):
-    """
-    Return the model. The return object is a numpy array with
-    its length equal to the length of qx.
-    """
-    model = self.F_trans() * self.F_cont()
-    return model
-
   def F_trans(self):
+    """
+    Transbilayer part of the ripple form factor
+    """
     rho_M = self.edp_par['rho_M'].value
     R_HM = self.edp_par['R_HM'].value
     X_h = self.edp_par['X_h'].value
@@ -182,7 +250,7 @@ class MDF(SDF):
   def __init__(self, h, k, F, q=None, qx=None, qz=None, D=58, lambda_r=140, 
                gamma=1.7, x0=100, A=20, f1=1, f2=0, rho_M=20, R_HM=2, X_h=20, 
                psi=0.087):
-    super().__init__(h, k, F, q, qx, qz, D, lambda_r, gamma, x0, A, 
+    super(MDF, self).__init__(h, k, F, q, qx, qz, D, lambda_r, gamma, x0, A, 
                      rho_M, R_HM, X_h, psi)
     self.edp_par['f1'].value = f1
     self.edp_par['f2'].value = f2
@@ -192,7 +260,25 @@ class MDF(SDF):
 
 ###############################################################################
 class SGF(Sawtooth):
-  pass
+  def __init__(self, h, k, F, q=None, qx=None, qz=None, D=58, lambda_r=140, 
+               gamma=1.7, x0=100, A=20, rho_M=20, R_HM=2, X_h=20, 
+               psi=0.087):
+    super(SDF, self).__init__(h, k, F, q, qx, qz, D, lambda_r, gamma, x0, A)
+    self.edp_par.add('rho_M', value=rho_M, vary=True)
+    self.edp_par.add('R_HM', value=R_HM, vary=True)
+    self.edp_par.add('X_h', value=X_h, vary=True)
+    self.edp_par.add('psi', value=psi, vary=True)
+  
+  def F_trans(self):
+    """
+    Transbilayer part of the ripple form factor
+    """
+    rho_M = self.edp_par['rho_M'].value
+    R_HM = self.edp_par['R_HM'].value
+    X_h = self.edp_par['X_h'].value
+    psi = self.edp_par['psi'].value  
+    arg = self.qz*X_h*np.cos(psi) - self.qx*X_h*np.sin(psi)
+    return rho_M * (R_HM*np.cos(arg) - 1)  
 
 
 ###############################################################################
@@ -210,52 +296,15 @@ class M1G(S1G):
   pass
     
 
-def Fourier_decomp(N=201, xmin=-100, xmax=100, zmin=-100, zmax=100):
-  rho_xz = []
-  xgrid = np.linspace(xmin, xmax, num=N)
-  zgrid = np.linspace(zmin, zmax, num=N)
-  for x in xgrid:
-    for z in zgrid:
-      tmp = phase * F * np.cos(qx*x+qz*z)
-      rho_xz.append([x, z, tmp.sum(axis=0)])
-  rho_xz = np.array(rho_xz, float)  
-  X = rho_xz[:,0]
-  Y = rho_xz[:,1]
-  Z = rho_xz[:,2]
-  X.shape = (N, N)
-  Y.shape = (N, N)
-  Z.shape = (N, N)
-  plt.contourf(X, Y, Z)
 
 
 
 
-def show_model():
-  model = SDF_model(params, qx, qz)
-  print(" h  k      qx     qz      q   model       F")
-  for a, b, c, d, e, f, g in zip(h, k, qx, qz, q, model, F):
-    print("{0: 1d} {1: 1d}  {2: .3f} {3: .3f} {4: .3f} {5: 7.2f} {6: 7.2f}"
-          .format(a, b, c, d, e, f, g))
 
 
-def show_profile(start, end):
-  """
-  Show Fourier decomposed EDP along a line connecting the start and end points
-  """
-  rho = []
-  x0, z0 = start
-  x1, z1 = end
-  N = 100 # number of points on which ED gets calculated
-  xpoints = np.linspace(x0, x1, N)
-  zpoints = np.linspace(z0, z1, N)
-  for x, z in zip(xpoints, zpoints):
-    tmp = phase * F * np.cos(qx*x+qz*z)
-    dist = np.sqrt((x-x0)**2 + (z-z0)**2)
-    rho.append([dist, tmp.sum(axis=0)])
-  rho = np.array(rho, float)
-  X = rho[:,0]
-  Y = rho[:,1]
-  plt.plot(X, Y)
+
+
+
   
   
 if __name__ == "__main__":
@@ -269,7 +318,7 @@ if __name__ == "__main__":
   sdf = SDF(h, k, F, q, qx=None, qz=None, D=58, lambda_r=140, gamma=1.7, x0=105, 
             A=20.3, rho_M=53.9, R_HM=2.21, X_h=20.2, psi=0.0868)   
   sdf.fit_lattice()
-  sdf.report_lattice()
+  #sdf.report_lattice()
   sdf.fit_edp()
   sdf.report_edp()
   
@@ -277,7 +326,7 @@ if __name__ == "__main__":
   mdf = MDF(h, k, F, q, qx=None, qz=None, D=58, lambda_r=140, gamma=1.7, 
             x0=103, A=20, f1=0.7, f2=0, rho_M=60, R_HM=2, X_h=20.4, psi=0.157)
   mdf.fit_lattice()
-  mdf.report_lattice()
+  #mdf.report_lattice()
   mdf.fit_edp()
   mdf.report_edp()
   
