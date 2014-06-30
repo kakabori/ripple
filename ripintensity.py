1eaa7aa0fe179dd95a5183093406f59d63710557
Kiyo <kakabori@andrew.cmu.edu>
2014-06-30 09:30:34 -0400

    report_model_F() method displays the calculated phase

--- ripintensity.py
+++ ripintensity.py
@@ -294,7 +294,7 @@ class BaseRipple(object):
     exp_F = exp_F / F_10 * 100
     model_F = model_F / F_10 * 100
     print(" h  k      q   model       F")
-    for a, b, c, d, e in zip(self.h, self.k, self.q, model_F, exp_F):
+    for a, b, c, d, e in zip(self.h, self.k, self.q, self.phase*model_F, exp_F):
       print("{0: 1d} {1: 1d} {2: .3f} {3: 7.2f} {4: 7.2f}".format(a, b, c, d, e))
       
   def report_model_I(self):
@@ -429,7 +429,7 @@ class BaseRipple(object):
   
   def _model_observed_I(self):
     """
-    Return the geometrically corrected observed intensity predicted by the 
+    Return Lorentz corrected intensity predicted by the 
     model form factor
     """
     global wavelength
@@ -503,7 +503,7 @@ class BaseRipple(object):
 
 ###############################################################################
 class Sawtooth(BaseRipple):
-  def __init__(self, h, k, q, I, sigma, D=58, lambda_r=140, gamma=1.7, 
+  def __init__(self, h, k, q, I, sigma, D=57.8, lambda_r=145, gamma=1.71, 
                x0=100, A=20):
     super(Sawtooth, self).__init__(h, k, q, I, sigma, D, lambda_r, gamma)
     self.edp_par = Parameters()
@@ -741,14 +741,14 @@ def F_T(h=1,k=0,D=57.94,lr=141.7,gamma=1.7174,rhom=51.38,rhm=2.2,xh=20.1,psi=5):
 
 if __name__ == "__main__":
   # read data to be fitted
-  infilename = 'intensity/ripple_082-085_2.dat'
+  infilename = 'intensity/085_ver1.dat'
   h, k, q, I, sigma, combined = read_data_5_columns(infilename)
 
 ###############################################################################
   # Work on SDF
   sdf = SDF(h, k, q, I, sigma, D=57.8, lambda_r=145.1, gamma=1.714, 
             x0=103, A=18.6, 
-            common_scale=1, R_HM=2.1, X_h=20.1, psi=0.08) 
+            common_scale=510, R_HM=2.1, X_h=20.1, psi=0.08) 
   sdf.set_combined_peaks(combined)
 #  sdf.set_mask(h=1, k=0, value=False)
 #  sdf.set_mask(h=2, k=0, value=False)
@@ -756,14 +756,14 @@ if __name__ == "__main__":
 #  sdf.set_mask(h=3, k=6, value=False)
 #  sdf.set_mask(h=4, k=0, value=False)
 #  sdf.fit_lattice()
-#  sdf.fit_edp()
-#  sdf.report_edp()
+  sdf.fit_edp()
+  sdf.report_edp()
 
 ###############################################################################
   # Work on MDF
   mdf = MDF(h, k, q, I, sigma, D=57.8, lambda_r=145.1, gamma=1.714, 
             x0=104, A=22, f1=1, f2=-5, 
-            common_scale=2, R_HM=2.2, X_h=20, psi=0.2) 
+            common_scale=1000, R_HM=2.2, X_h=20, psi=0.2) 
   mdf.set_mask(h=1, k=0, value=False)
   mdf.set_mask(h=2, k=0, value=False)
 #  mdf.fit_edp()
@@ -771,12 +771,12 @@ if __name__ == "__main__":
 
 ###############################################################################
   # Work on S1G
-  s1g = S1G(h, k, q, I, sigma, D=57.94, lambda_r=141.7, gamma=1.7174,
+  s1g = S1G(h, k, q, I, sigma, D=57.8, lambda_r=145.1, gamma=1.714, 
             x0=103, A=18.5, 
             rho_H1=10.77, Z_H1=20.86, sigma_H1=3.43,
-            rho_M=9.23, sigma_M=1.67, psi=0.0873, common_scale=1)
+            rho_M=9.23, sigma_M=1.67, psi=0.0873, common_scale=1000)
   s1g.set_combined_peaks(combined)
-  s1g.fit_lattice()
+#  s1g.fit_lattice()
   s1g.edp_par['rho_H1'].vary = False
   s1g.edp_par['sigma_H1'].vary = False
   s1g.edp_par['rho_M'].vary = False
@@ -789,8 +789,8 @@ if __name__ == "__main__":
   m1g = M1G(h, k, q, I, sigma, D=57.94, lambda_r=141.7, gamma=1.7174,
             x0=103, A=19.0, f1=0.6, f2=-1, 
             rho_H1=10.77, Z_H1=19.3, sigma_H1=3.43,
-            rho_M=9.23, sigma_M=1.67, psi=0.157, common_scale=1)
-  m1g.fit_lattice()
+            rho_M=9.23, sigma_M=1.67, psi=0.157, common_scale=1000)
+#  m1g.fit_lattice()
   m1g.edp_par['x0'].vary = True
   m1g.edp_par['A'].vary = True
   m1g.edp_par['f1'].vary = True  
@@ -802,17 +802,17 @@ if __name__ == "__main__":
   m1g.edp_par['sigma_M'].vary = False 
   m1g.edp_par['psi'].vary = True
   m1g.edp_par['common_scale'].vary = True
-  m1g.fit_edp()
-  m1g.report_edp()   
+#  m1g.fit_edp()
+#  m1g.report_edp()   
 
 ###############################################################################
   # Work on S2G
   s2g = S2G(h, k, q, I, sigma, D=58, lambda_r=144, gamma=1.7,
             x0=100, A=20, rho_H1=9.91, Z_H1=19.45, sigma_H1=2.94,
             rho_H2=7.27, Z_H2=23.47, sigma_H2=1.47, 
-            rho_M=10.91, sigma_M=1.83, psi=0.1, common_scale=1)
+            rho_M=10.91, sigma_M=1.83, psi=0.1, common_scale=1000)
   s2g.set_combined_peaks(combined)
-  s2g.fit_lattice()
+#  s2g.fit_lattice()
   s2g.edp_par['rho_H1'].vary = False
   s2g.edp_par['sigma_H1'].vary = False
   s2g.edp_par['rho_H2'].vary = False
