d252497caf1d57667811cbc19bf3cc65a1bd1266
kiyo <kakabori@andrew.cmu.edu>
2014-09-23 11:36:03 -0400

    added free/fix checkbuttons

--- ripplefitGUI.py
+++ ripplefitGUI.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from Tkinter import *
+#from ttk import *
+import ttk
 import IPython 
 from ripplefit import *
 
@@ -51,67 +53,50 @@ class ripplefitGUI(object):
         right2 = Frame(top)
         right2.pack(side='left')
 
-        self.lattice_pars = {}    
-        self.lattice_pars['D'] = DoubleVar()
-        self.lattice_pars['D'].set(57.8)
-        self.lattice_pars['lambda_r'] = DoubleVar()
-        self.lattice_pars['lambda_r'].set(145.0)
-        self.lattice_pars['gamma'] = DoubleVar()
-        self.lattice_pars['gamma'].set(1.714)
+        # Initialize associated variables
+        # Call as lattice_pars['D']['val'].get() etc.
+        self.lattice_pars = {}
+        for key, i in zip(['D', 'lambda_r', 'gamma'], [57.8, 145.0, 1.714]):
+            self.lattice_pars[key] = {'val': DoubleVar(), 'vary': IntVar()}    
+            self.lattice_pars[key]['val'].set(i)
+            self.lattice_pars[key]['vary'].set(False)
+            
         self.contour_pars = {}
-        self.contour_pars['A'] = DoubleVar()
-        self.contour_pars['A'].set(22)
-        self.contour_pars['xM'] = DoubleVar()
-        self.contour_pars['xM'].set(90)
-        self.contour_pars['f1'] = DoubleVar()
-        self.contour_pars['f1'].set(1.5)
-        self.contour_pars['f2'] = DoubleVar()
-        self.contour_pars['f2'].set(-20)
+        for key, i in zip(['A', 'xM', 'f1', 'f2'], [22.0, 90.0, 1.5, -20.0]):
+            self.contour_pars[key] = {'val': DoubleVar(), 'vary': IntVar()}
+            self.contour_pars[key]['val'].set(i)
+            self.contour_pars[key]['vary'].set(True)
+            
         self.transbilayer_pars = {}
-        self.transbilayer_pars['rho_H1'] = DoubleVar()
-        self.transbilayer_pars['rho_H1'].set(9.91)
-        self.transbilayer_pars['Z_H1'] = DoubleVar()
-        self.transbilayer_pars['Z_H1'].set(20)
-        self.transbilayer_pars['sigma_H1'] = DoubleVar()
-        self.transbilayer_pars['sigma_H1'].set(2.94)
-        self.transbilayer_pars['rho_H2'] = DoubleVar()
-        self.transbilayer_pars['rho_H2'].set(7.27)
-        self.transbilayer_pars['Z_H2'] = DoubleVar()
-        self.transbilayer_pars['Z_H2'].set(20)
-        self.transbilayer_pars['sigma_H2'] = DoubleVar()
-        self.transbilayer_pars['sigma_H2'].set(1.47)
-        self.transbilayer_pars['rho_M'] = DoubleVar()
-        self.transbilayer_pars['rho_M'].set(10.91)
-        self.transbilayer_pars['sigma_M'] = DoubleVar()
-        self.transbilayer_pars['sigma_M'].set(1.83)
-        self.transbilayer_pars['psi'] = DoubleVar()
-        self.transbilayer_pars['psi'].set(0.1)
-        self.transbilayer_pars['common_scale'] = DoubleVar()
-        self.transbilayer_pars['common_scale'].set(3)
-
+        for key, i in zip(['rho_H1', 'Z_H1', 'sigma_H1', 'rho_H2', 'Z_H2', 
+                           'sigma_H2', 'rho_M', 'sigma_M', 'psi', 
+                           'common_scale'], [9.94, 20.0, 2.94, 7.27, 20.0, 
+                           1.47, 10.91, 1.83, 0.1, 3]):
+            self.transbilayer_pars[key] = {'val': DoubleVar(), 'vary': IntVar()}
+            self.transbilayer_pars[key]['val'].set(i)
+            self.transbilayer_pars[key]['vary'].set(True)
+                    
         for key in ['D', 'lambda_r', 'gamma']:
             #lattice_pars[key].trace("w", update_lattice_pars)
             tmp, self.row_counter['lattice'] = \
-                self.textentry(left, self.lattice_pars[key], key, 
+                self.textentry(left, self.lattice_pars[key]['val'], 
+                               self.lattice_pars[key]['vary'], key, 
                                self.row_counter['lattice'])
 
         for key in ['A', 'xM', 'f1', 'f2']:
             #contour_pars[key].trace("w", update_contour_pars)
             tmp, self.row_counter['contour'] = \
-                self.textentry(middle, self.contour_pars[key], key,
+                self.textentry(middle, self.contour_pars[key]['val'], 
+                               self.contour_pars[key]['vary'], key,
                                self.row_counter['contour'])
 
         for key in ['rho_H1', 'Z_H1', 'sigma_H1', 'rho_H2', 'Z_H2', 'sigma_H2', 
                     'rho_M', 'sigma_M', 'psi', 'common_scale']:
             #transbilayer_pars[key].trace("w", update_transbilayer_pars)    
             tmp, self.row_counter['transbilayer'] = \
-                self.textentry(right, self.transbilayer_pars[key], key,
+                self.textentry(right, self.transbilayer_pars[key]['val'], 
+                               self.transbilayer_pars[key]['vary'], key,
                                self.row_counter['transbilayer'])
-              
-        #self.text_box = Text(right2, wrap=None, height=11, width=50)
-        #self.text_box.grid(column=0, row=0, columnspan=2, sticky='NSWE', 
-        #                   padx=5, pady=5)
-        #sys.stdout = StdoutRedirector(self.text_box)
         
         fit_btn = Button(bottom, text='Fit', command=self.fit_model)
         fit_btn.pack(side='left')
@@ -122,10 +107,18 @@ class ripplefitGUI(object):
         quit_button.pack(side='left', pady=5, fill='x')
         #self.master.bind('<q>', self.quit)
         
-    def textentry(self, parent, variable, label, row_counter):
-        L = Label(parent, text=label)
-        L.grid(column=0, row=row_counter, sticky='w')
-        widget = Entry(parent, textvariable=variable, width=8)
+        n = ttk.Notebook(bottom)
+        f1 = ttk.Frame(n); # first page, which would get widgets gridded into it
+        f2 = ttk.Frame(n); # second page
+        n.add(f1, text='One')
+        n.add(f2, text='Two')
+        
+    def textentry(self, parent, text_var, check_var, label, row_counter):
+        #L = Label(parent, text=label)
+        #L.grid(column=0, row=row_counter, sticky='w')
+        C = Checkbutton(parent, text=label, variable=check_var)
+        C.grid(column=0, row=row_counter, sticky='w')
+        widget = Entry(parent, textvariable=text_var, width=8)
         widget.grid(column=1, row=row_counter)
         row_counter += 1
         return widget, row_counter
@@ -134,36 +127,29 @@ class ripplefitGUI(object):
         self.parent.destroy()
     
     def fit_model(self):
+        """
+        Update the M2G parameter objects with the current values in the GUI,
+        perform a NLSQ optimization, and then update the GUI with the best
+        fit values.
+        """
+        # Update Parameter objects with values and free/fix from the GUI
         for key in self.lattice_pars:
-            self.m2g.latt_par[key].value = self.lattice_pars[key].get()
+            self.m2g.latt_par[key].value = self.lattice_pars[key]['val'].get()
         for key in self.contour_pars:
-            self.m2g.edp_par[key].value = self.contour_pars[key].get()
+            self.m2g.edp_par[key].value = self.contour_pars[key]['val'].get()
+            self.m2g.edp_par[key].vary = bool(self.contour_pars[key]['vary'].get())
         for key in self.transbilayer_pars:
-            self.m2g.edp_par[key].value = self.transbilayer_pars[key].get()
+            self.m2g.edp_par[key].value = self.transbilayer_pars[key]['val'].get()
+            self.m2g.edp_par[key].vary = bool(self.transbilayer_pars[key]['vary'].get())                     
         self.m2g.fit_edp()
         self.m2g.report_edp()
         for key in self.contour_pars:
-            self.contour_pars[key].set(self.m2g.edp_par[key].value)
+            self.contour_pars[key]['val'].set(self.m2g.edp_par[key].value)
         for key in self.transbilayer_pars:
-            self.transbilayer_pars[key].set(self.m2g.edp_par[key].value)
+            self.transbilayer_pars[key]['val'].set(self.m2g.edp_par[key].value)
 
 
-    
-    
-def update_lattice_pars(*args):
-    global lattice_pars
-    for key in lattice_pars:
-        m2g.latt_par[key].value = lattice_pars[key].get()
 
-def update_contour_pars(*args):
-    global contour_pars
-    for key in contour_pars:
-        m2g.edp_par[key].value = contour_pars[key].get()
-            
-def update_transbilayer_pars(*args):
-    global transbilayer_pars
-    for key in transbilayer_pars:
-        m2g.edp_par[key].value = transbilayer_pars[key].get() 
 
 
        
